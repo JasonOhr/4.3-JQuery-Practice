57371d2e5873bcd3ec30307d53fcede62197ba0f
Jason Ohr <jason.ohr@gmail.com>
2015-05-27 21:01:56 -0400

    completed the accordion effect and the hover effect tho they aren't as smooth as I'd like.  Also, I cut the images out of the .png in the assignment demo so I don't have all the colors'

--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,36 @@
 (function(){
   'use strict';
-  //hello
+    function openPanels(){
+      $('.my-files .tasks').slideDown();
+    }
   $(document).ready(function(){
     $('body').prepend(JST['application']());
+    var $category = $('li.category');
+    var panels = $('.accordion  li.tasks').hide();
+    openPanels();
+    $category.hover(function(){
+      $(this).addClass('temp-active');
+    }, function (){
+          $(this).removeClass('temp-active');
+        }
+    );
+    $category.click(function(){
+      var $this = $(this);
+
+      if($this.hasClass("active")){
+        console.log($this);
+        $this.removeClass('active').addClass('in-active')
+        panels.slideUp();
+      }else {
+        console.log('im here ',$this);
+        panels.slideUp();
+        $('.category.active').removeClass('active').addClass('in-active');
+        panels.slideUp();
+        $this.removeClass('in-active').addClass('active')
+            $this.siblings().slideDown();
+      }
+    });
+
   });
 
 })();
